307f81f69d7de11f70ac8913bde7a2dccc8f2e13
igapon50 <igapon@gmail.com>
2021-01-14 00:43:42 +0900

    ファイル名連番を修正

--- python/Web_scraping/folder01Rename.bat
+++ python/Web_scraping/folder01Rename.bat
@@ -5,15 +5,13 @@ pushd folder01
 setlocal enabledelayedexpansion
 set prefix=
 set pad=3
-set num=0
 for %%F in (*) do (
  rem このバッチファイル自身を対象に含めない
  if not %%F==%~nx0 (
-  set padnum=00000000!num!
+  set padnum=00000000%%~nF
   set extention=%%~xF
   ren %%F !padnum:~-%pad%!!prefix!_%%F
   echo %%F → !padnum:~-%pad%!!prefix!_%%F
-  set /a num+=1
  )
 )
 endlocal
