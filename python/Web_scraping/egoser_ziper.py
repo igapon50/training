e0aaedb00586b05924671514be5440ab72265993
igapon <igapon@gmail.com>
2022-12-30 11:05:33 +0900

    追加 egoser_ziper.py

---
+++ python/Web_scraping/egoser_ziper.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import copy
+
+from webFileListHelper import *
+from crawling import *
+
+
+if __name__ == '__main__':  # インポート時には動かない
+    # 引数チェック
+    my_name = 'カタマラン'
+    if 2 == len(sys.argv):
+        # Pythonに以下の2つ引数を渡す想定
+        # 0は固定でスクリプト名
+        # 1.エゴサ対象の名前
+        my_name = sys.argv[1]
+    elif 1 == len(sys.argv):
+        # 引数がなければ、クリップボードからURLを得る
+        paste_str = pyperclip.paste()
+        if 0 < len(paste_str):
+            my_name = paste_str
+    # クリップボードが空なら、デフォルトURLを用いる
+    else:
+        print('引数が不正です。')
+        print(msg_error_exit)
+        sys.exit()
+    site_url = f'https://www.google.com/search?q={my_name}'
+    site_selectors = {
+        'page_urls': [
+            (By.XPATH,
+             '//*[@id="hdtb-msb"]/div[1]/div/div[2]/a',
+             lambda el: el.get_attribute("href")
+             ),
+        ]
+    }
+    page_selectors = {
+        'title_jp': [(By.XPATH,
+                      '//*[@id="REsRA"]',
+                      lambda el: el.get_attribute("value")),
+                     ],
+        'title_en': [(By.XPATH,
+                      '//*[@id="REsRA"]',
+                      lambda el: el.get_attribute("value")),
+                     ],
+        'image_urls': [(By.XPATH,
+                       '//*[@id="islrg"]/div[1]/div/a[1]/div[1]/img',
+                        lambda el: el.get_attribute("src")),
+                       ],
+    }
+    image_selectors = {
+        'image_urls': [(By.XPATH,
+                       '//*[@id="islrg"]/div[1]/div/a[1]/div[1]/img',
+                        lambda el: el.get_attribute("src")),
+                       ],
+    }
+    crawling = Crawling(site_url, site_selectors, 'egoser_ziper.txt')
+    crawling_items = crawling.get_crawling_items()
+    page_urls = []
+    if 'page_urls' in crawling_items:
+        page_urls = crawling_items['page_urls']
+    for page_url in page_urls:
+        print(page_url)
+        if crawling.is_url_included_exclusion_list(page_url):
+            crawling.move_url_from_page_urls_to_exclusion_urls(page_url)
+            crawling.save_text()
+            continue
+        items = crawling.scraping(page_url, page_selectors)
+        image_urls = crawling.take_out(items, 'image_urls')[0:20]  # 先頭の20個は、DataURIで表示される
+        title = Crawling.validate_title(items, 'title_jp', 'title_en')
+        url_title = ChromeDriverHelper.fixed_file_name(page_url)
+        # フォルダがなかったらフォルダを作る
+        os.makedirs(WebFileListHelper.work_path, exist_ok=True)
+        target_file_name = os.path.join(WebFileListHelper.work_path, f'{title}：{url_title}.html')
+        print(title)
+        if not os.path.exists(target_file_name):
+            if image_urls:
+                print(image_urls)
+                web_file_list = WebFileListHelper(image_urls)
+                web_file_list.download_chrome_driver()
+                if not web_file_list.make_zip_file():
+                    sys.exit()
+                if not web_file_list.rename_zip_file(title):
+                    if not web_file_list.rename_zip_file(f'{title}：{url_title}'):
+                        sys.exit()
+                web_file_list.delete_local_files()
+                # 成功したらチェック用ファイルを残す
+                ChromeDriverHelper().save_source(target_file_name)
+            # page_urlsからexclusion_urlsにURLを移して保存する
+        crawling.move_url_from_page_urls_to_exclusion_urls(page_url)
+        crawling.save_text()
+    print('crawling-end')
